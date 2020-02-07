7aa55420b9fb72cab1aa160db0676c4559d93ed8
Robert Cabrera <robertcabrera4871@gmail.com>
2020-02-06 21:12:56 -0500

    Min requirement, functioning calculator, not operations buttons

--- app/src/main/java/com/example/bookcalculatorassignment/MainActivity.kt
+++ app/src/main/java/com/example/bookcalculatorassignment/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.bookcalculatorassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onButtonClick(v: View){
+        val button = v as Button
+        val bText = button.text.toString()
+        val value = bText.toInt()
+        total += value
+        textView.text = total.toString()
     }
 }
