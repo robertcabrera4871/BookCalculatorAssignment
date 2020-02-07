4f007992f241d4b51faff5fc52a57d42e62b081b
Robert Cabrera <robertcabrera4871@gmail.com>
2020-02-06 20:09:31 -0500

    Assignment from Mobile Programming first commit

---
+++ app/src/main/java/com/example/bookcalculatorassignment/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bookcalculatorassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var total = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
